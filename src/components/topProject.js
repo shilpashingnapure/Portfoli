1e5c48cd737559601a65bf93c9e3348d590ace8d
Shilpa Shingnapure <69167064+shilpashingnapure@users.noreply.github.com>
2022-03-02 23:55:30 +0530

    top projects

---
+++ src/components/topProject.js
@@ -0,0 +1,15 @@
+import "../styles/topProject.css";
+function Toproject() {
+  return (
+    <div className="top-Projects">
+      <div>Hellow</div>
+      <div className="project-imges">
+        <div className="card img-1"></div>
+        <div className="card img-2"></div>
+        <div className="card img-3"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Toproject;
